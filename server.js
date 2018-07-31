680d732252d9a754f65a23059c37399f07e66943
Paul Miller <paul@battlestardigital.com>
2018-07-31 16:31:01 -0400

    Work continues; moving towards promises throughout code

--- server.js
+++ server.js
@@ -37,6 +37,70 @@ const logEntry = (level, message) => {
 
 logEntry('info', `${appName} started`);
 
+// SQL
+const con = mysql.createConnection({
+	host: config.database.host,
+	user: config.database.user,
+	password: config.database.password,
+	database: config.database.database,
+	multipleStatements: true
+});
+
+con.connect((err) => {
+	if (err) {
+		logEntry('error', `Error attempting to connnect to database: ${err}`);
+		return;
+	}
+	logEntry('info', 'Connected to database');
+});
+
+const logDownload = (filename) => {
+	con.query(
+	  `SET @import_log_id = 0; CALL logFileImport('${filename}', 2, 1, 1, @import_log_id ); SELECT @import_log_id`,
+	  (err, results) => {
+		if (err) {
+			logEntry('error', `Error logging download in database: ${err}`);
+			return;
+		}
+
+	   // console.log(results[2][0]['@import_log_id']);
+	  }
+	)
+}
+
+// const wasFileImported = (filename) => {		
+// 	con.query(`CALL wasFileImported('${filename}')`, (err, results) => {
+// 		console.log('03 Running query');	
+// 		if (err) {
+// 			console.log('04a error path');
+// 			logEntry('error', `Error searching database: ${err}`);
+// 			return false;
+// 		}
+// 		console.log('04b no error');
+// 		result = results[0][0]['importCheck'];
+// 		console.log('05 result variable = ', result);
+// 		return result;
+// 	});	
+// }
+
+const wasFileImported = (filename) => {		
+	return new Promise( (resolve, reject) => {
+		con.query(`CALL wasFileImported('${filename}')`, (err, results) => {
+			console.log('03 Running query');	
+			if (err) {
+				console.log('04a error path');
+				logEntry('error', `Error searching database: ${err}`);
+				return false;
+			}
+			console.log('04b no error');
+			result = results[0][0]['importCheck'];
+			console.log('05 result variable = ', result);
+			return result;
+		})
+	});
+}
+
+
 // Set up SFTP variables and config
 let sftp = new client();
 
@@ -68,18 +132,49 @@ const localUrl = config.local.downloadDestination;
 			fileList.push(val['name']);
 		});
 
-		// TODO: https://www.sitepoint.com/using-node-mysql-javascript-client/
-		// Start here. Let's loop through the array, check to see if file has already been imported
-		// If so, drop from array
-
-		//sftp.fastGet(ordersUrl + 'daily.csv', localUrl + 'daily.csv');
 		fileList.forEach((file) => {
 			if (file.endsWith('.csv')) {
-				logEntry('info', `found file ${file}`);
-				sftp.fastGet(ordersUrl + file, localUrl + file);
-				logEntry('info', `downloaded file ${file}`);
+				console.log('01 File ends with csv');
+				logEntry('info', `found file at CRM FTP site: ${file}`);
 
-				// INSERT a log entry into the new sales_file_import database
+				console.log('02 Calling importCheck function with filename ', file);
+				var importCheck = wasFileImported(file).then(() => {
+					console.log('06 importCheck variable: ', importCheck);	
+				});
+				
+
+
+
+
+
+				//console.log('importCheck: ', importCheck);
+				if ( importCheck === true) {
+					logEntry('info', `${file} previously imported. Skipping.`);
+				} else {
+					sftp.fastGet(ordersUrl + file, localUrl + file);
+					logEntry('info', `downloaded file ${file}`);
+					logDownload(file);
+				};					
+
+				// downloadFile();
+
+				// async function downloadFile() {
+				// 	console.log('01 File ends with csv');
+				// 	logEntry('info', `found file at CRM FTP site: ${file}`);
+
+				// 	console.log('02 Calling importCheck function with filename ', file);
+				// 	var importCheck = await wasFileImported(file);
+				// 	console.log('06 importCheck variable: ', importCheck);
+
+				// 	//console.log('importCheck: ', importCheck);
+				// 	if ( importCheck === true) {
+				// 		logEntry('info', `${file} previously imported. Skipping.`);
+				// 	} else {
+				// 		sftp.fastGet(ordersUrl + file, localUrl + file);
+				// 		logEntry('info', `downloaded file ${file}`);
+				// 		logDownload(file);
+				// 	};					
+				// }
 			}
 		})
 
